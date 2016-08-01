7d1f5b1bd3634f684e6a204137e10d8e8279ffc8
saurabhbhatt <saurabh.statistics@gmail.com>
2016-08-01 23:39:23 +0530

    Add more design

---
+++ pohajunction/js/pohajunction.js
@@ -0,0 +1,47 @@
+$('[data-toggle="tooltip"]').tooltip();
+$('[data-toggle="popover"]').popover();
+function showHeight() {
+  var $height = $(window ).height() / 9;
+  $('.wrapper-hero .centralise').css('margin-top', $height);
+}
+
+$( window ).resize(function(){
+  showHeight();
+});
+
+$(document).ready(function(){
+  /* Bootstrap UI Elements*/
+  showHeight();
+  /* Parallax Backgrounds */
+  $('.wrapper[data-type="background"]').each(function(){
+    var $bgobj = $(this); // assigning the object
+    $(window).scroll(function() {
+      var yPos = -($(window).scrollTop() / $bgobj.data('speed')); 
+      var coords = '100%' + yPos + 'px';
+      // Move the background
+      $bgobj.css({ backgroundPosition: coords });
+    });
+  });
+});
+
+$(function() {
+  //caches a jQuery object containing the header element
+  var header = $(".navbar");
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 100) {
+      header.addClass("scroll");
+    } else {
+      header.removeClass("scroll");
+    }
+  });
+});
+
+function isNumber(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
+}
