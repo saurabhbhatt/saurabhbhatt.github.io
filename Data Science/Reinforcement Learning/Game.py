7c6dd926a82c53ad5d5985440be83d5ec764c116
saurabhbhatt <saurabh.statistics@gmail.com>
2018-03-09 11:21:05 +0530

    Assignment 1

---
+++ Data Science/Reinforcement Learning/Game.py
@@ -0,0 +1,13 @@
+"""
+===============================================================================
+Objective: Catch a ball release from a random locationfrom the top of the screen
+with the paddle at the bottom of the screen by moving the paddle horizontally 
+using the left and right arrow keys
+===============================================================================
+Winning Criterion: The player wins if the paddle can catch the ball and loses 
+if the balls falls off the screen before the paddle gets to it.
+===============================================================================
+"""
+
+# Importing Libraries
+from __future__ import division, print_function
